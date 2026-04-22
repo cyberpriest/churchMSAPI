2ebe7e04a8535323f093648a8891d0a27f8553ed
cyberpriest <joshuatimi41@gmail.com>
2026-04-22 20:15:39 +0100

    'a'

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,10 @@ from Routers.servicerouter import service_router
 app = FastAPI(title=' CHRUCH MANAGEMENT SYS. ')
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://b4bf9a33-eb59-4655-b543-fd5238a46652-00-2veq4rcu396gy.riker.replit.dev'] , # Adjust this in production to restrict origins
+    allow_origins=[
+        'localhost:3000',
+   ' https://church-management-system--joshuatimi41.replit.app/',
+    'https://b4bf9a33-eb59-4655-b543-fd5238a46652-00-2veq4rcu396gy.riker.replit.dev'] , # Adjust this in production to restrict origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
