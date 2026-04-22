ad4db7ce4baa5774a5897ac365585290128fad62
cyberpriest <joshuatimi41@gmail.com>
2026-04-22 21:19:12 +0100

    'bat'

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
 
-   ' https://church-management-system--joshuatimi41.replit.app',
+   'https://church-management-system--joshuatimi41.replit.app',
     'https://b4bf9a33-eb59-4655-b543-fd5238a46652-00-2veq4rcu396gy.riker.replit.dev'] , # Adjust this in production to restrict origins
     allow_credentials=True,
     allow_methods=["*"],
